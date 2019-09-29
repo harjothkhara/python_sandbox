50166d03930fa15eefb18ae9efe29e53014924a9
harjoth <harjoth.khara@gmail.com>
2019-09-29 12:48:20 -0700

    files

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -1,1 +1,26 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# Open a file
+myFile = open('myfile.txt', 'w')  # created a file
+
+# Get some info
+print('Name: ', myFile.name)  # Name:  myfile.txt
+# Is Closed :  False # means is it closed within our script
+print('Is Closed : ', myFile.closed)  # False
+print('Opening Mode: ', myFile.mode)  # Opening Mode:  w
+
+# Write to file
+myFile.write('I love Python')
+myFile.write(' and JavaScript')
+myFile.close()
+
+# Append to file
+myFile = open('myfile.txt', 'a')
+myFile.write(' I also like PHP')
+myFile.close()
+print('Is Closed : ', myFile.closed)  # True
+
+# Read from file
+myFile = open('myfile.txt', 'r+')
+text = myFile.read(6)  # read 6 characters
+print(text)
